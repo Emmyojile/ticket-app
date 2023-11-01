7847a89d3413274096abb653e596f3088900b8fa
Emmyojile <emmaojile99@gmail.com>
2023-10-31 21:38:01 -0800

    form layout

---
+++ app/(components)/TicketForm.jsx
@@ -0,0 +1,70 @@
+"use client";
+import { useState } from "react";
+
+const TicketForm = () => {
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    console.log("submitted");
+  };
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "not started",
+    category: "Hardware Problem",
+  };
+
+  const [formData, setFormData] = useState(startingTicketData);
+  return (
+    <div className="flex justify-center">
+      <form
+        className="flex flex-col gap-3 w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/3"
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <h3>Create Your Ticket</h3>
+        <label>Title</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+        <label>Description</label>
+        <textarea
+          id="description"
+          name="description"
+          onChange={handleChange}
+          required={true}
+          value={formData.description}
+          rows={"5"}
+        />
+        <label>Category</label>
+
+        <select
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option value="Hardware Problem">Hardware Problem</option>
+          <option value="Software Problem">Software Problem</option>
+          <option value="Project">Project</option>
+        </select>
+      </form>
+    </div>
+  );
+};
+
+export default TicketForm;
